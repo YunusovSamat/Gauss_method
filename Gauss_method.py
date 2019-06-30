4624b921eeaef293e98e9ee57030dc01dbe3a917
YunusovSamat <44583676+YunusovSamat@users.noreply.github.com>
2019-06-30 23:10:53 +0300

    NumPy library. OOP style

--- Gauss_method.py
+++ Gauss_method.py
@@ -7,24 +7,30 @@ class GaussMethod:
     def __init__(self, count_row, count_col, arr):
         self.n = count_row
         self.m = count_col
-        self.mtx = np.zeros((n, m), dtype="O")
+        self.x_vector = np.zeros(self.m-1, dtype="O")
+        self.mtx = np.zeros((self.n, self.m), dtype="O")
 
         self.fill_matrix(arr)
         self.step_method()
+        self.set_x_vector()
 
     def fill_matrix(self, arr):
-        for i in range(n):
+        for i in range(self.n):
             self.mtx[i] = np.array(list(map(Fraction, arr[i])), dtype="O")
 
     def step_method(self):
         def conv_to_one(vector, k):
-            return vector / vector[k]
+            if vector[k] == 0:
+                print("Что-то пошло не так")
+                exit(1)
+            else:
+                return vector / vector[k]
 
         def conv_to_zero(vector1, vector2, k):
             return vector1 * vector2[k] - vector2
 
-        print(self)
-        for i in range(n):
+        self.show_matrix()
+        for i in range(self.n):
             if self.mtx[i][i] != 1:
                 for row, elem in enumerate(self.mtx[i:, i]):
                     if abs(elem) == 1:
@@ -32,45 +38,98 @@ class GaussMethod:
                             self.mtx[i + row].copy() * elem,
                             self.mtx[i].copy()
                         )
-                        print(self)
+                        self.show_matrix()
                         break
             self.mtx[i] = conv_to_one(self.mtx[i], i)
             for j in range(i + 1, n):
                 self.mtx[j] = conv_to_zero(self.mtx[i], self.mtx[j], i)
-            print(self)
+            self.show_matrix()
+
+    def set_x_vector(self):
+        for i in range(self.n-1, -1, -1):
+            for j in range(i+1, self.n):
+                self.x_vector[i] -= self.mtx[i][j] * self.x_vector[j]
+            self.x_vector[i] += self.mtx[i][-1]
+
+        for i in range(self.m-1):
+            self.x_vector[i] = float(self.x_vector[i])
+
+    def show_matrix(self):
+        for row in self.mtx:
+            print("(", end="")
+            for elem in row[:-1]:
+                print("{0:^4}".format(str(elem)), end="")
+            print("|{0:^4})".format(str(row[-1])))
+        print()
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i > len(self.x_vector)-1:
+            raise StopIteration
+        else:
+            value = self.x_vector[self.i]
+            self.i += 1
+            return value
+
+    def __len__(self):
+        return len(self.x_vector)
 
     def __str__(self):
-        show = ""
-        for row in self.mtx:
-            show += "("
-            for elem in row[:-1]:
-                show += "{0:^4}".format(str(elem))
-            show += "|{0:^4})\n".format(str(row[-1]))
-        return show
+        return str(self.x_vector)
+
+    def iscorrentindex(self, i):
+        if isinstance(i, (int, slice)):
+            if isinstance(i, int) and (i > self.__len__()-1):
+                raise IndexError
+        else:
+            raise TypeError
+
+    def __getitem__(self, item):
+        self.iscorrentindex(item)
+        return self.x_vector[item]
+
+    def __setitem__(self, item, value):
+        self.iscorrentindex(item)
+        self.x_vector[item] = value
+
+    def __delitem__(self, item):
+        self.iscorrentindex(item)
+        del self.x_vector[item]
+
+    def __contains__(self, value):
+        return value in self.x_vector
 
 
 if __name__ == '__main__':
-    # n = int(input("Введите кол-во строк: "))
-    n = 4
-    m = n + 1
+    print("Введите кол-во строк: ", end="")
+    while True:
+        s = input()
+        if s.isdigit():
+            n = int(s)
+            break
+        else:
+            print("Введенное значение не корректно.\n"
+                  "Введите кол-во строк: ", end="")
 
-    # t = [
-    #     [3, 2, -5, -1],
-    #     [1, 2, -1, 9],
-    #     [2, -1, 3, 13]
-    # ]
+    print("Введите данные матрицы:")
+    array = []
+    for i in range(n):
+        while True:
+            row_arr = input().split()
+            if len(row_arr) == n+1:
+                try:
+                    array.append(list(map(float, row_arr)))
+                except ValueError:
+                    print("Введенное значение не корректно."
+                          "\nВведите строку еще раз:")
+                else:
+                    break
+            else:
+                print("Количество столбцов не равно", n+1,
+                      "\nВведите строку еще раз:")
 
-    t2 = [
-        [2, 5, 4, 1, 20],
-        [1, 3, 2, 1, 11],
-        [2, 10, 9, 7, 40],
-        [3, 8, 9, 2, 37]
-    ]
-
-    # print("Введите данные матрицы:")
-    # for i in range(n):
-    #     row_arr = input().split()
-    #     mtx[i] = list(map(int, row_arr))
-
-    gm = GaussMethod(n, m, t2)
-
+    gm = GaussMethod(n, n+1, array)
+    print(gm)
